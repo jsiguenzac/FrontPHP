d1e1bb889ed49bd214356b7cfefa4eec963bd290
jsiguenzac <joelsantiago.pes@gmail.com>
2022-05-20 00:46:23 -0500

    tablaSpanishFIN

--- carrera.php
+++ carrera.php
@@ -163,7 +163,7 @@
 });
 tabladata = $('#tableCarrera').DataTable({
   "language": {
-                "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
+                "url": "https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
             }
 });
 
